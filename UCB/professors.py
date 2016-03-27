16688d1e8869f07eae905e6d2b3a49d578eb4497
nicci1771 <xiaoqiqi1771@126.com>
2016-03-27 23:03:31 +0800

    add UCB

--- UCB/professors.py
+++ UCB/professors.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python2
+#coding=utf-8
 
 import urllib2
 import HTMLParser
 import sys
 import xml.dom.minidom as minidom
+from htmlentitydefs import entitydefs
 
-fileLocation = './cmu_images'
+fileLocation = './ucb_images'
 
 class MyParser(HTMLParser.HTMLParser):
     def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
         self.hasinfo = False
-        self.namelist = []
+        self.infolist = []
         self.hastd = False
         self.hastr = False
         self.hasref = False
-        self.imagelist = []
+        self.hascolspan = False
+        self.tempinfo = [[],[],[]]
+        #image, website, name, title 
     def handle_starttag(self, tag, attrs):
         if tag == 'tr':
             self.hastr = True
         if tag == 'td':
             self.hastd = True
-        if tag == 'a' and self.hastd == True:
+            for key, value in attrs:
+                if key == 'colspan' and value == '2':
+                    self.hascolspan = True
+        if tag == 'a' and self.hascolspan == True:
             for key, value in attrs:
                 if key == 'href':
-                    self.tempinfo[0] = value
+                    self.tempinfo[1] = 'http://www.eecs.berkeley.edu' + value
                     self.hasref = True
         if self.hastd == True and tag == 'img':
             for key, value in attrs:
@@ -33,71 +40,57 @@ class MyParser(HTMLParser.HTMLParser):
                     imgFileName = fileLocation + '/'+value.split('/')[-1]
                     imgFile = open(imgFileName, 'w')
                     imgFile.write(imgContent)
-                    self.image_path = imgFileName
-
+                    self.tempinfo[0] = imgFileName
+    def handle_entityref(self, name):
+        if entitydefs.has_key(name):
+            self.handle_data(entitydefs[name])
+        else:
+            self.handle_data('&'+name+';')
     def handle_data(self, text):
         if self.hasref:
-            self.namelist.append(text)
+            self.tempinfo[2].append(text)
     def handle_endtag(self, tag):
         if tag == 'tr':
             self.hastr = False
+            if self.tempinfo[0] != []:
+                self.infolist.append(self.tempinfo)
+                self.tempinfo = [[],[],[]]
         if tag == 'td':
             self.hastd = False
-        if self.hasref == True:
+        if self.hasref == True: 
             self.hasref = False
-
-rootUrl = 'http://www-cs.stanford.edu/directory/faculty'
+            self.hascolspan = False
+rootUrl = 'http://www.eecs.berkeley.edu/Faculty/Lists/CS/list.shtml'
 response = urllib2.urlopen(rootUrl)
 html = response.read()
 my = MyParser()
 my.feed(html)
-    
-fout_xml = file('CMU.xml','w')
+
+fout_xml = file('UCB.xml','w')
 doc = minidom.Document()
 institution = doc.createElement("institution")
 doc.appendChild(institution)
 website_no = 0
 for i in range(len(my.infolist)):
-    if len(my.infolist[i][1]) == 0:
-        continue
     professor = doc.createElement("professor")
     institution.appendChild(professor)
     
-    name = my.infolist[i][1][0][0]
+    name = ''
+    for namepart in my.infolist[i][2]:
+        name += namepart
     namenode = doc.createElement("name")
     namenode.appendChild(doc.createTextNode(name))
     professor.appendChild(namenode)
     
-    office = ''
-    for x in my.infolist[i][1][2]:
-        office = x+' '
-    if office != '':
-        officenode = doc.createElement("office")
-        officenode.appendChild(doc.createTextNode(office[:-1]))
-        professor.appendChild(officenode)
+    website = my.infolist[i][1]
+    websitenode = doc.createElement("website")
+    websitenode.appendChild(doc.createTextNode(website))
+    professor.appendChild(websitenode)
     
-    phone = my.infolist[i][1][1]
-    if len(phone) != 0:
-        phonenode = doc.createElement("phone")
-        phonenode.appendChild(doc.createTextNode(phone[0]))
-        professor.appendChild(phonenode)
-    
-    website = my.infolist[i][0]
-    if website != []:
-        websitenode = doc.createElement("website")
-        websitenode.appendChild(doc.createTextNode(website))
-        professor.appendChild(websitenode)
-        try:
-            single_response = urllib2.urlopen(website)
-            single_html = response.read()
-            single = SingleParser()
-            single.feed(single_html)
-            if single.image_path != '':
-                imagenode = doc.createElement("image")
-                imagenode.appendChild(doc.createTextNode(image_path))
-                professor.appendChild(imagenode)
-        except:
-            pass
+    image = my.infolist[i][0]
+    imagenode = doc.createElement("image")
+    imagenode.appendChild(doc.createTextNode(image))
+    professor.appendChild(imagenode)
     
     institution.appendChild(professor)
 doc.writexml(fout_xml, "\t", "\t", "\n")
