4cd981810b50534cb909d16b6d13720a356ba70b
nicci1771 <xiaoqiqi1771@126.com>
2016-03-28 06:11:47 +0800

    add CMU images and filter

--- CMU/professors.py
+++ CMU/professors.py
@@ -10,6 +10,46 @@ import glob
 
 fileLocation = './cmu_images'
 
+class SingleParser(HTMLParser.HTMLParser):
+    def __init__(self, website):
+        HTMLParser.HTMLParser.__init__(self)
+        self.image_path = ''
+        self.website = website
+    def handle_starttag(self, tag, attrs):
+        if self.image_path == '' and (tag == 'img' or tag =='IMG'):
+            for key, value in attrs:
+                if key == 'src' or key == 'SRC':
+                    if value[-3:] == 'jpg' or value[-4:] == 'jpeg' or value[-3:] == 'png' or 'png' in value:
+                        if value[:4] != 'http':
+                            tempvalue = self.website+'/'+value
+                        try:
+                            imgContent = urllib2.urlopen(tempvalue).read()
+                        except:
+                            tempvalue = 'http://www.cs.cmu.edu'+value
+                            imgContent = urllib2.urlopen(tempvalue).read()
+                        imgFileName = fileLocation+'/'+tempvalue.split('/')[-1]
+                        imgFile = open(imgFileName, 'w')
+                        imgFile.write(imgContent)
+                        self.image_path = imgFileName
+                            
+class WebParser(HTMLParser.HTMLParser):
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+        self.personal_website = ''
+        self.haspw = False
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            for key, value in attrs:
+                if key == 'class' and value == 'field field-name-field-personal-website field-type-link-field field-label-inline clearfix':
+                    self.haspw = True
+        if tag == 'a' and self.haspw == True:
+            for key, value in attrs:
+                if key == 'href':
+                    self.personal_website = value
+    def handle_endtag(self, tag):
+       if self.personal_website != '':
+           self.haspw = False
+
 class MyParser(HTMLParser.HTMLParser):
     def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
@@ -104,7 +144,7 @@ doc = minidom.Document()
 institution = doc.createElement("institution")
 doc.appendChild(institution)
 
-for no in range(1, 11):
+for no in range(0, 10):
     rootUrl = 'https://www.scs.cmu.edu/directory/all?term_node_tid_depth=10571&page='+str(no)
     response = urllib2.urlopen(rootUrl)
     html = response.read()
@@ -142,19 +182,36 @@ for no in range(1, 11):
             professor.appendChild(phonenode)
     
         website = my.infolist[i][6]
-        websitenode = doc.createElement("website")
-        websitenode.appendChild(doc.createTextNode(website))
-        professor.appendChild(websitenode)
-    
-        #image = my.infolist[i][0]
-        #imagenode = doc.createElement("image")
-        #imagenode.appendChild(doc.createTextNode(image))
-        #professor.appendChild(imagenode)
-    
+        try:
+            web_response = urllib2.urlopen(website)
+            web_html = web_response.read()
+            web = WebParser()
+            web.feed(web_html)
+            print web.personal_website
+            if web.personal_website != '':
+                websitenode = doc.createElement("website")
+                websitenode.appendChild(doc.createTextNode(website))
+                professor.appendChild(websitenode)
+            
+                try:
+                    single_response = urllib2.urlopen(web.personal_website, timeout=20)
+                    single_html = single_response.read()
+                    single = SingleParser(web.personal_website)
+                    single.feed(single_html)
+                    if single.image_path:
+                        image = single.image_path
+                        imagenode = doc.createElement("image")
+                        imagenode.appendChild(doc.createTextNode(image))
+                        professor.appendChild(imagenode)
+                except:
+                    pass
+        except:
+            pass
         institution.appendChild(professor)
 
 doc.writexml(fout_xml, "\t", "\t", "\n")
 
 fout_xml.close()
+
 
 
